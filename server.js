8af8eb80a7f6b52b933d7f7e537f7a59a7f3dfff
Abner Persio <binhopersio@gmail.com>
2022-10-08 11:20:23 -0300

    fix: removes old files

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-const routes = require("./src/routes");
-app.use(express.json());
-
-app.use(routes);
-
-app.listen(process.env.PORT || 3000);
--- src/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-
-routes.get('/ping', function (req, res) {
-    return res.send('pong');
-});
-
-module.exports = routes;
